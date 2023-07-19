d4c7cf9b2995e1c94c22b3599f8e2d005a6ed183
Amutheswaran <amutheswaran@outlook.com>
2023-07-20 05:23:16 +0530

    logical problems added

---
+++ 00_misc/swap2nums.py
@@ -0,0 +1,13 @@
+
+a = 4
+b = 10
+
+print(f'a is {a}')
+print(f'b is {b}')
+
+c = b
+b = a
+a = c
+
+print(f'a is {a}')
+print(f'b is {b}')
---
+++ 00_misc/tables.py
@@ -0,0 +1,7 @@
+m = 10
+n = 7
+
+
+for m in range(1, m+1):
+    t = n*m
+    print(f'{n}x{m}={t}')
