0abde9280e39ab8dce33e5a9603003201a83317e
Amutheswaran <amutheswaran@outlook.com>
2023-07-21 00:02:24 +0530

    misc ex workout

---
+++ 00_misc/read_data.py
@@ -0,0 +1,5 @@
+# with open('E:/GitHub Projects/python-complete/00_misc/readandsplit.py', 'r') as file:
+#     data = data = file.read().rstrip()
+
+text_as_string = open('readandsplit.py', 'r').read()
+print(text_as_string)
---
+++ 00_misc/readandsplit.py
@@ -0,0 +1,12 @@
+# f = open('E:/GitHub Projects/python-complete/00_misc/readandsplit.py',
+#          encoding='utf8')
+# file_contents = f.readlines()
+
+# for var in file_contents:
+#     print(var)
+
+
+file1 = open("E:/GitHub Projects/python-complete/00_misc/readandsplit.py", "r+")
+print("Output of Read function is ")
+print(file1.read())
+print()
