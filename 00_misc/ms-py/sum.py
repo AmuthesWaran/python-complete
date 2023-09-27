0185ba9199d8040771bd2fd75fb37294a9b6b292
Amutheswaran <amutheswaran@outlook.com>
2023-09-27 20:26:59 +0530

    ms notes 2 mods completed

---
+++ 00_misc/ms-py/sum.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+sum = 1 + 2
+product = sum * 2
+print(product)
+print('this show in the console')
+
+floatNum = 8.9
+print(type(floatNum))
+
+print(date.today())
+
+# print("Today's date is " + date.today())
+
+print("Today's date is " + str(date.today()))
+
+print("1" + 2)
