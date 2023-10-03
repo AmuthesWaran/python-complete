3b48723917d9bc2ede137166d0e9a44f1ed525cc
Amutheswaran <amutheswaran@outlook.com>
2023-10-03 15:49:40 +0530

    fuzzy logic added

---
+++ 00_misc/fuzzymatch/fuzzy-match.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from fuzzywuzzy import fuzz, process
+
+
+firstname = pd.read_csv('00_misc/fuzzymatch/firstname.csv')
+fullname = pd.read_csv('00_misc/fuzzymatch/fullname.csv')
+
+# print(firstname)
+# print(fullname)
+
+
+# Function to compute fuzzy match score
+def compute_fuzzy_match(source, target):
+    return fuzz.ratio(source, target)
+
+
+# Initialize an empty DataFrame to store the matching results
+matching_results = []
+
+
+# Iterate through records in file1_df
+for index1, row1 in firstname.iterrows():
+    # Adjust this based on your file structure
+    source_record = row1['firstname']
+
+    # Iterate through records in file2_df
+    for index2, row2 in fullname.iterrows():
+        # Adjust this based on your file structure
+        target_record = row2['fullname']
+
+        # Compute the fuzzy match score
+        match_score = compute_fuzzy_match(source_record, target_record)
+
+        # Add the match to the results DataFrame if it meets a certain threshold
+        if match_score >= 0:  # Adjust the threshold as needed
+            matching_results.append(
+                {'File1_Record': source_record, 'File2_Record': target_record, 'FuzzyMatchScore': match_score})
+
+matching_results_df = pd.DataFrame(matching_results)
+
+print(matching_results_df)
+
+matching_results_df.to_csv('matching_results.csv', index=False)
