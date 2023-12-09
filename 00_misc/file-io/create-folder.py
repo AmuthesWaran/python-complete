087bd4498de83e7afa1abb4dc4176a3422e231b1
amutheswaran <amutheswaran@outlook.com>
2023-12-09 19:30:11 +0530

    date dir

---
+++ 00_misc/file-io/create-folder.py
@@ -0,0 +1,21 @@
+import os
+from datetime import datetime
+
+# Get today's date
+today_date = datetime.now().date()
+# print(today_date)
+
+# Format the date as a string without any special characters
+folder_name = today_date.strftime("%d%m%Y")
+# print(folder_name)
+
+# Specify the path where you want to create the folder
+# folder_path = f'/d/python-complete/00_misc/file-io/{folder_name}'
+folder_path = folder_name
+
+# Create a new folder/directory
+try:
+    os.makedirs(folder_path)
+    print(f"Folder '{folder_path}' created successfully.")
+except OSError as e:
+    print(f"Error: {e}")
